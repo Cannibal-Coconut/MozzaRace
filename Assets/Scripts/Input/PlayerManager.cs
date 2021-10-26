e577c60de8fe80b7664fac73a987d0198153f37d
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2021-10-26 05:02:44 +0200

    Add Player Attack

---
+++ Assets/Scripts/Input/PlayerManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[Serializable]
+public class JumpInputEvent : UnityEvent<float> {}
+
+[Serializable]
+public class StartAttackInputEvent : UnityEvent<Vector2> {}
+
+[Serializable]
+public class EndAttackInputEvent : UnityEvent<Vector2> {}
+
+
+/**
+ * Set the configurations to be able to interact with the player and disable the UI input
+ */
+[DefaultExecutionOrder(-1)]
+public class PlayerManager : MonoBehaviour
+{
+    private Controls _controls;
+
+    public JumpInputEvent jumpInputEvent;
+    public StartAttackInputEvent startAttackInputEvent;
+    public EndAttackInputEvent endAttackInputEvent;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _controls = new Controls();
+    }
+    
+    private void Start()
+    {
+        _controls.Player.Jump.performed += OnJumpPerformed;
+        _controls.Player.Jump.canceled += OnJumpPerformed;
+
+        _controls.Player.AttackContact.started += OnAttackStarted;
+        _controls.Player.AttackContact.canceled += OnAttackEnded;
+    }
+
+    private void OnEnable()
+    {
+        _controls.Player.Enable();
+        _controls.UI.Disable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Disable();
+    }
+
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        if (jumpInputEvent == null) return;
+        var jumped = context.ReadValue<float>();
+        jumpInputEvent.Invoke(jumped);
+    }
+
+    private void OnAttackStarted(InputAction.CallbackContext context)
+    {
+        if (startAttackInputEvent == null) return;
+        var startPoint = _controls.Player.AttackPosition.ReadValue<Vector2>();
+
+        var startPointWorldCoords = Utils.ScreenToWorld(_camera, startPoint);
+        
+        startAttackInputEvent.Invoke(startPointWorldCoords);
+    }
+
+    private void OnAttackEnded(InputAction.CallbackContext context)
+    {
+        if (endAttackInputEvent == null) return;
+        var endPoint = _controls.Player.AttackPosition.ReadValue<Vector2>();
+
+        var endPointWorldCoords = Utils.ScreenToWorld(_camera, endPoint);
+        
+        endAttackInputEvent.Invoke(endPointWorldCoords);
+    }
+
+    public Vector2 AttackPosition()
+    {
+        return Utils.ScreenToWorld(_camera, _controls.Player.AttackPosition.ReadValue<Vector2>());
+    }
+}
