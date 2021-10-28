abeb6bc8b62b32588eb9982a0440c0d3c6c7e673
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-10-28 19:25:45 +0200

    Roller integrado

--- Assets/Scripts/Obstacles/Roller.cs
+++ Assets/Scripts/Obstacles/Roller.cs
@@ -6,22 +6,23 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Roller : MonoBehaviour
 {
-
+    [Header("Settings")]
     [SerializeField] [Range(0, 10)] float _speed;
+    [SerializeField] [Range(0, 20)] float _flippedSpeed;
     [SerializeField] [Range(0, 5)] float _bounceHeight;
-    Rigidbody2D _rigidbody;
 
-    int _sign = -1;
+    Rigidbody2D _rigidbody;
+    bool _flipped;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-
     }
 
     private void Start()
     {
-        float verticalSpeed = Mathf.Sqrt(Mathf.Abs(2 * Physics2D.gravity.y * _bounceHeight));
+        //h*m*g = 1/2*m*(v^2) => v = (2*g*h)^0.5; _bounceHeight is h, just calculate speed for such height
+        float verticalSpeed = Mathf.Pow(Mathf.Abs(2 * Physics2D.gravity.y * _bounceHeight), 0.5f);
 
         var desiredVelocity = _rigidbody.velocity;
         desiredVelocity.y = verticalSpeed;
@@ -30,14 +31,22 @@ public class Roller : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(new Vector2(_speed * _sign, 0));
+        if (_flipped)
+        {
+            _rigidbody.velocity = new Vector2(_flippedSpeed, _rigidbody.velocity.y);
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(-_speed, _rigidbody.velocity.y);
+        }
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetContact(0).collider.GetComponent<PlayerAttack>())
+        if (collision.GetComponent<PizzaLaunch>())
         {
-            _sign *= -1;
+            _flipped = true;
         }
     }
 
