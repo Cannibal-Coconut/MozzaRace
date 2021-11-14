9522faea5a7206c8de86704e977c70fa9e0f8668
dexaxi <dexaxi12@gmail.com>
2021-11-14 23:36:44 +0100

    Background Asset Downscaling

--- Assets/Scripts/Backgrounds/Scroller.cs
+++ Assets/Scripts/Backgrounds/Scroller.cs
@@ -5,22 +5,21 @@ using UnityEngine;
 public class Scroller : MonoBehaviour
 {
 
-    private BoxCollider2D _collider;
     private float _width;
     private float _scrollSpeed = 8f;
+    [SerializeField] private float _parallaxEffect;
 
+    private void Start() {  
+              
+        _width = GetComponent<SpriteRenderer>().bounds.size.x;
 
-    private void Start() {
-        _collider = GetComponent<BoxCollider2D>();
-        
-        _width = _collider.size.x;
-
+        _parallaxEffect = 1/_parallaxEffect;
         
 
     }
 
     private void Update() {
-        transform.position  = (Vector2) transform.position - new Vector2(_scrollSpeed * Time.deltaTime,0);
+        transform.position  = (Vector2) transform.position - new Vector2(_scrollSpeed * _parallaxEffect* Time.deltaTime,0);
         if(transform.position.x < -_width){
             
             Vector2 resetPosition = new Vector2(_width*2f,0);
