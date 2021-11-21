6fa86504b681b607df402c5ae643848f66b68e56
Carlos <carlos.garcia.sanz05@gmail.com>
2021-11-21 02:12:41 +0100

    [FEAT] Game Pressets Created

--- Assets/Scripts/Spawners/Spawnable.cs
+++ Assets/Scripts/Spawners/Spawnable.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Spawnable : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] [Range(0, 10f)] float _timeForNextSpawn;
+    [SerializeField] [Range(0, 50f)] float _timeForNextSpawn;
 
     public float timeForNextSpawn
     {
