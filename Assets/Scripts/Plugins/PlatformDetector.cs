75ea8954033c6e0a6f6cf42263caf658df10cc26
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2021-11-22 01:47:05 +0100

    [FIX] Platform Detector not working Android and IOS devices

--- Assets/Scripts/Plugins/PlatformDetector.cs
+++ Assets/Scripts/Plugins/PlatformDetector.cs
@@ -8,10 +8,13 @@ public static class PlatformDetector
 {
     [DllImport("__Internal")]
     private static extern bool IsMobile();
+
     public static bool IsPlatformMobile()
     {
 #if !UNITY_EDITOR && UNITY_WEBGL
             return IsMobile();
+#elif UNITY_ANDROID || UNITY_IOS
+        return true;
 #endif
         return false;
     }
