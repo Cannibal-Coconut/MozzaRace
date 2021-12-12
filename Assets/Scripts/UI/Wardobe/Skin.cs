c676d29cf90218cda5cbd3ae3ba7506a617d048a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-12-12 02:31:31 +0100

    [FIX] Persistent Shop

--- Assets/Scripts/UI/Wardobe/Skin.cs
+++ Assets/Scripts/UI/Wardobe/Skin.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 [Serializable]
 public class Skin
 {
-    public int value { get; private set; }
-    public SkinHandler.SkinEnum skin { get; private set; }
+    public int value;
+    public SkinEnum skinID;
     public bool purchased;
     public Sprite sprite;
-    public Skin(SkinHandler.SkinEnum s, int value)
+
+    public Skin(SkinEnum s, int value)
     {
-        this.skin = s;
+        this.skinID = s;
         this.value = value;
         SkinHandler sk = GameObject.FindObjectOfType<SkinHandler>();
-        if(sk.GetSprite(s) !=null) this.sprite =sk.GetSprite(s);
-        else this.sprite = sk.GetSprite(this.skin);
+        if (sk.GetSprite(s) != null) this.sprite = sk.GetSprite(s);
+        else this.sprite = sk.GetSprite(s);
     }
 
 }
